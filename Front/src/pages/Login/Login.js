95dd1b0b0623b6a0e812d5c464f1b0f9f78022b0
chaewon <89545462+gchaewon@users.noreply.github.com>
2022-07-29 05:18:44 +0900

    Feat: Login 기능 구현 업데이트

--- Front/src/pages/Login/Login.js
+++ Front/src/pages/Login/Login.js
@@ -1,7 +1,29 @@
-import db from '../../db.json';
+// import data from '../../db.json';
 
-document.getElementById('LoginContainer').onsubmit = function () {
-	var inputID = this.id.value;
-	var inputPW = this.pw.value;
-	db.users.map(user => user.id == inputID);
+// document.querySelector('.GoMainBtn').addEventListener('click', () => {
+// 	const id = 'admin';
+// 	const password = '1234';
+// 	if (id == document.querySelector('#ID').value) {
+// 		if (password == document.querySelector('#PW').value) {
+// 			alert('환영합니다!');
+// 			//document.write("환영합니다!");
+// 		} else {
+// 			alert('비밀번호가 맞지 않습니다.');
+// 			//document.write("비밀번호가 맞지 않습니다.");
+// 		}
+// 	} else {
+// 		alert('아이디 혹은 비밀번호가 맞지 않습니다.');
+// 		//document.wrtie("아이디 혹은 비밀번호가 맞지 않습니다.");
+// 	}
+// });
+
+import axios from '../../../node_modules/axios/dist/axios.min.js';
+
+const getUsers = () => {
+	axios.get(`http://localhost:8888/users`).then(res => {
+		console.log(res.data);
+	});
 };
+document.querySelector('.GoMainBtn').addEventListener('click', () => {
+	getUsers();
+});
